9136efa2be15e00160c8d86d37093f23a622cb34
Arina Nazarchuk <nazarchuk.arina@gmail.com>
2024-11-19 00:48:28 +0200

    up

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,9 @@ import StarryBackground from "./StarryBackground/StarryBackground";
 const App = () => {
   return (
     <div style={{ position: "relative", minHeight: "100vh" }}>
-      {/* Звездный фон */}
       <StarryBackground />
-
-      {/* Контент поверх фона */}
       <div style={{ position: "relative", zIndex: 1 }}>
         <h1>Добро пожаловать в мое приложение!</h1>
-        <p>Это текст поверх анимации звездного фона.</p>
-        {/* Добавь сюда другие компоненты */}
       </div>
     </div>
   );
